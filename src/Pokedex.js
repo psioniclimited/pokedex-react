830f23ca722380d3f723b944831c6d8d58b4474e
sabbir-anwar <protik26@gmail.com>
2020-11-02 18:30:39 +0600

    Route setup || Project Setup

---
+++ src/Pokedex.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Pokedex = () => {
+  return (
+    <div>
+      This is the pokedex
+    </div>
+  )
+}
---
+++ src/Pokemon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Pokemon = () => {
+  return (
+    <div>
+      Pokemen Page
+    </div>
+  )
+}
